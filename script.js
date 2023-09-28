4a5550775994795850c3f16a2e8ea799bb12e2ab
Roodini <roodinijoaquin@gmail.com>
2023-09-28 08:46:36 -0700

    Changed various things, including initial grid, now we can draw on the grid when we hover our mouse we have a button that removes the previous grid and replaces it with a new one plus we can adjust the size to whatever we want as long as its less than 100 one thing left to do is make it so the inner divs adjust to the width of 960px of the container, and it doesnt expand the container.

--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 const container = document.querySelector('.container');
-let numOfGrid = prompt("Enter the dimensions you would like for your grid", "");
+const newGridBtn = document.querySelector('.new-grid');
+const initialGridValue = 16;
 
 
 const gridCreate = (numOfGrid) => {
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
     for(let i = 0; i < numOfGrid; i++){
         const cols = document.createElement('div');
         cols.classList.add('grid-row');
         for(let j = 0; j < numOfGrid; j++){
             const gridBox = document.createElement('div');
+            const widthAndHeight = 960 / numOfGrid;
             gridBox.classList.add('grid-box');
+            gridBox.style.width = `${widthAndHeight}px`;
+            gridBox.style.height = `${widthAndHeight}px`;
             cols.append(gridBox);
+            gridBox.addEventListener('mouseover', () => {
+                gridBox.style.backgroundColor = "black";
+            })
         }
         container.appendChild(cols);
     }
 }
 
-gridCreate(numOfGrid);
+newGridBtn.addEventListener('click', () => {
+
+    let newGridSize = Number(prompt("Enter the dimensions you would like for your grid", ""));
+
+    while(newGridSize > 100){
+        newGridSize = Number(prompt("You must enter a number less than 100", ""));
+    }
+    gridCreate(newGridSize);
+})
+
+gridCreate(initialGridValue);
 
 
